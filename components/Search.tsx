1788a079f1744e020708f947063de216d1a56d70
Maxim Chernyshov <dsgnrvd@gmail.com>
2022-07-30 03:12:30 +0400

    Revert "mutate history every minute"
    This reverts commit 2b0592e7e11405af089a0a8586999955416bf6c6.

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,27 +1,17 @@
 import { FormElement, Input, Loading, Row, Spacer } from "@nextui-org/react";
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { WeatherResponse } from "types/Weather";
 import useSWR from 'swr'
 import { fetcher } from "utils/fetcher";
 import useDebounce from "hooks/useDebounce";
 
-const thousandMilliseconds = 1e3;
-const minute = thousandMilliseconds * 60;
-
 export const Search = () => {
-  const [lastSavedHistory, setLastSavedHistory] = useState<undefined | WeatherResponse>()
-
   const [search, setSearch] = useState('');
   
-  const [queryParamsHistory, setQueryParamsHistory] = useState<string[]>([]);
-
   const queryParams = useDebounce(`city=${search}`, 1000);
 
-  const { data, error, mutate } = useSWR<WeatherResponse>(`/api/v1/getCurrentWeather?${queryParams}`, fetcher, {
+  const { data, error } = useSWR<WeatherResponse>(`/api/v1/getCurrentWeather?${queryParams}`, fetcher, {
     revalidateOnMount: false,
-    onSuccess: (data) => {
-      setLastSavedHistory(data)
-    }
   })
 
   const isDataLoading = !data;
@@ -30,31 +20,6 @@ export const Search = () => {
     setSearch(e.target.value);
   };
 
-  useEffect(() => {
-    setQueryParamsHistory(prev => {
-      return [...new Set([...prev, queryParams])]
-    });
-  }, [queryParams]);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setQueryParamsHistory(prev => {
-        prev.forEach(async (e) => 
-          {
-            const response = await fetcher(`/api/v1/getCurrentWeather?${e}`);
-            mutate(
-              response,
-            );
-        })
-        return [...prev];
-      })
-    }, minute);
-
-    return () => {
-      clearInterval(interval);
-    }
-  }, [mutate]);
-  
   return (
     <div>
       <Row>
@@ -65,7 +30,7 @@ export const Search = () => {
         {search.length > 0 && <>
           {isDataLoading && <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}><Loading /></div>}
           {data && <code style={{ width: '100%', wordBreak: 'break-all' }}>
-            {JSON.stringify(lastSavedHistory)}
+            {JSON.stringify(data)}
           </code>}
           {error &&  <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>Something happened</div>}
         </>}
